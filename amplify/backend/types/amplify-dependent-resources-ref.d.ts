541bca986128def9c5624945f9116b03c797e0ff
Robert Froeschl <rwfroeschl@gmail.com>
2024-08-16 18:16:59 -0500

    added graphQL to project

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifytest": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
