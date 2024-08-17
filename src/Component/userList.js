482c766eee5f48842675bc531f6bd5d875b47c32
Robert Froeschl <rwfroeschl@gmail.com>
2024-08-17 11:43:15 -0500

    front-end configuration / forgot to save

---
+++ src/Component/userList.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import { API, graphqlOperation } from 'aws-amplify';
+import { listUsers } from './graphql/queries';
+
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+
+  const fetchUsers = async () => {
+    try {
+      const userData = await API.graphql(graphqlOperation(listUsers));
+      setUsers(userData.data.listUsers.items);
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      {users.map(user => (
+        <div key={user.id}>
+          <h3>{user.name}</h3>
+          <p>{user.career}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default UserList;
